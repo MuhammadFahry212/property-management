34466c51561b59f6adf44c2ec9174b5e2ac265c6
fahri sugiarto <fahrimuhammad212@gmail.com>
2023-05-14 22:50:32 +0700

    Implemented user handling using AOP

--- src/main/java/com/mycompany/propertymanagement/repository/UserRepository.java
+++ src/main/java/com/mycompany/propertymanagement/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.mycompany.propertymanagement.entity.PropertyEntity;
 import com.mycompany.propertymanagement.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<UserEntity,Long> {
 
+    Optional<UserEntity> findByOwnerEmailAndPassword(String email, String password);
+    Optional<UserEntity> findByOwnerEmail(String email);
 }
