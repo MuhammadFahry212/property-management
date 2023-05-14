43f9123d6f98062003f3c026b25af2290698f94f
fahri sugiarto <fahrimuhammad212@gmail.com>
2023-05-14 16:24:12 +0700

    test

--- src/main/java/com/mycompany/propertymanagement/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/mycompany/propertymanagement/service/impl/PropertyServiceImpl.java
@@ -44,6 +44,7 @@ public class PropertyServiceImpl implements PropertyService {
     //GetAllData
     @Override
     public List<PropertyDTO> getAllProperties() {
+        System.out.println("Inside service ");
         System.out.println("Inside service "+dummy);
         System.out.println("Inside service "+dbUrl);
         System.out.println("Inside service "+dummy);
